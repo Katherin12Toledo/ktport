882345fa37001a3a38564e58e236bc4e4a86985b
Katherin12Toledo <katherine12toledo@gmail.com>
2015-10-22 14:44:18 -0400

    added all these files, making the portfolio gallery

---
+++ jsfolder/main.js
@@ -0,0 +1,19 @@
+// Carousel Auto-Cycle
+  $(document).ready(function() {
+    $('.carousel').carousel({
+      interval: 6000
+    })
+  });
+
+	// // Activate Carousel
+	// $("#myCarousel").carousel();
+
+	// // Enable Carousel Indicators
+	// $(".item").click(function(){
+	//     $("#myCarousel").carousel(1);
+	// });
+
+	// // Enable Carousel Controls
+	// $(".left").click(function(){
+	//     $("#myCarousel").carousel("prev");
+	// });
